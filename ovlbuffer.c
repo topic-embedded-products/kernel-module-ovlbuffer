ecfd1c046526c96b117b02d5a75eea3ae5da436a
Mike Looijmans <mike.looijmans@topic.nl>
2017-01-11 13:31:43 +0100

    Initial commit

---
+++ ovlbuffer.c
@@ -0,0 +1,275 @@
+/*
+ * AXI VDMA Overlay buffer driver
+ * Based on: vdmfb.c
+ *
+ * Copyright (C) 2017 Topic Embedded Products
+ * Author: Mike Looijmans <mike.looijmans@topic.nl>
+ *
+ * Licensed under GPL-2.
+ *
+ * Example devicetree contents:
+	axi_overlay_mixer: axi_overlay_mixer@43c10000 {
+		compatible = "topic,axi-overlay-mixer";
+		reg = <0x43c10000 0x10000>;
+		dmas = <&axi_vdma_0 0>, <&axi_vdma_0 1>;
+		dma-names = "frombuf", "tobuf";
+		hsize = <960>;
+		vsize = <960>;
+	};
+ */
+
+#include <linux/dma-mapping.h>
+#include <linux/errno.h>
+#include <linux/init.h>
+#include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/mm.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/dmaengine.h>
+//#include <linux/dma/xilinx_dma.h>
+
+/* Register locations */
+#define OVLBUF_CONTROL	0x00
+
+/* Register control flags */
+#define OVLBUF_CONTROL_ENABLE 1
+
+struct ovlbuf_dev {
+	struct device *device;
+	void __iomem *regs;
+	/* Physical and virtual addresses of framebuffer */
+	phys_addr_t fb_phys;
+	void __iomem *fb_virt;
+	/* VDMA handle */
+	struct dma_chan *dma_tobuf;
+	struct dma_interleaved_template *dma_template_tobuf;
+	struct dma_chan *dma_frombuf;
+	struct dma_interleaved_template *dma_template_frombuf;
+	/* Configuration */
+	u32 hsize; /* In bytes */
+	u32 vsize; /* Scanlines */
+	u32 frames;
+};
+
+static inline u32 ovlbuf_readreg(struct ovlbuf_dev *fbdev, loff_t offset)
+{
+	return ioread32(fbdev->regs + offset);
+}
+
+static inline void ovlbuf_writereg(struct ovlbuf_dev *fbdev, loff_t offset, u32 data)
+{
+	iowrite32(data, fbdev->regs + offset);
+}
+
+static int ovlbuf_setupfb(struct ovlbuf_dev *fbdev)
+{
+	struct dma_async_tx_descriptor *desc;
+	struct dma_interleaved_template *dma_template = fbdev->dma_template_frombuf;
+	//struct xilinx_vdma_config vdma_config;
+
+	/* Disable display */
+	ovlbuf_writereg(fbdev, OVLBUF_CONTROL, 0);
+
+	dmaengine_terminate_all(fbdev->dma_tobuf);
+	dmaengine_terminate_all(fbdev->dma_frombuf);
+
+	/* Setup VDMA address etc */
+	//memset(&vdma_config, 0, sizeof(vdma_config));
+	//vdma_config.park = 1;
+	//xilinx_vdma_channel_set_config(fbdev->dma, &vdma_config);
+
+	/*
+	* Interleaved DMA:
+	* Each interleaved frame is a row (hsize) implemented in ONE
+	* chunk (sgl has len 1).
+	* The number of interleaved frames is the number of rows (vsize).
+	* The icg in used to pack data to the HW, so that the buffer len
+	* is fb->piches[0], but the actual size for the hw is somewhat less
+	*/
+	dma_template->dir = DMA_MEM_TO_DEV;
+	dma_template->src_start = fbdev->fb_phys;
+	/* sgl list have just one entry (each interleaved frame have 1 chunk) */
+	dma_template->frame_size = 1;
+	/* the number of interleaved frame, each has the size specified in sgl */
+	dma_template->numf = fbdev->vsize;
+	dma_template->src_sgl = 1;
+	dma_template->src_inc = 1;
+	/* vdma IP does not provide any addr to the hdmi IP */
+	dma_template->dst_inc = 0;
+	dma_template->dst_sgl = 0;
+	/* horizontal size */
+	dma_template->sgl[0].size = fbdev->hsize;
+	/* the vdma driver seems to look at icg, and not src_icg */
+	dma_template->sgl[0].icg = 0; /*  stride - hsize */
+
+	desc = dmaengine_prep_interleaved_dma(fbdev->dma_frombuf, dma_template, 0);
+	if (!desc) {
+		pr_err("Failed to prepare from-buffer DMA descriptor\n");
+		return -ENOMEM;
+	}
+	dmaengine_submit(desc);
+	dma_async_issue_pending(fbdev->dma_frombuf);
+
+	dma_template = fbdev->dma_template_tobuf;
+	dma_template->dir = DMA_DEV_TO_MEM;
+	dma_template->src_start = fbdev->fb_phys;
+	/* sgl list have just one entry (each interleaved frame have 1 chunk) */
+	dma_template->frame_size = 1;
+	dma_template->numf = fbdev->vsize;
+	dma_template->src_sgl = 0;
+	dma_template->src_inc = 0;
+	dma_template->dst_inc = 1;
+	dma_template->dst_sgl = 1;
+	dma_template->sgl[0].size = fbdev->hsize;
+	dma_template->sgl[0].icg = 0; /*  stride - hsize */
+
+	desc = dmaengine_prep_interleaved_dma(fbdev->dma_tobuf, dma_template, 0);
+	if (!desc) {
+		pr_err("Failed to prepare to-buffer DMA descriptor\n");
+		return -ENOMEM;
+	}
+	dmaengine_submit(desc);
+	dma_async_issue_pending(fbdev->dma_tobuf);
+
+	/* Enable output */
+	ovlbuf_writereg(fbdev, OVLBUF_CONTROL, OVLBUF_CONTROL_ENABLE);
+
+	return 0;
+}
+
+
+static int ovlbuf_probe(struct platform_device *pdev)
+{
+	struct ovlbuf_dev *fbdev;
+	struct resource *res;
+	struct device_node *np = pdev->dev.of_node;
+	u32 fbsize;
+	int ret;
+
+	fbdev = devm_kzalloc(&pdev->dev, sizeof(*fbdev), GFP_KERNEL);
+	if (!fbdev)
+		return -ENOMEM;
+
+	platform_set_drvdata(pdev, fbdev);
+	fbdev->device = &pdev->dev;
+
+	fbdev->dma_template_tobuf = devm_kzalloc(&pdev->dev,
+		sizeof(struct dma_interleaved_template) +
+		sizeof(struct data_chunk), GFP_KERNEL);
+	if (!fbdev->dma_template_tobuf)
+		return -ENOMEM;
+	fbdev->dma_template_frombuf = devm_kzalloc(&pdev->dev,
+		sizeof(struct dma_interleaved_template) +
+		sizeof(struct data_chunk), GFP_KERNEL);
+	if (!fbdev->dma_template_frombuf)
+		return -ENOMEM;
+
+	fbdev->hsize = 960;
+	fbdev->vsize = 960;
+	fbdev->frames = 3;
+
+	ret = of_property_read_u32(np, "hsize", &fbdev->hsize);
+	if (ret)
+		dev_err(&pdev->dev, "Missing hsize property, assume 960\n");
+	ret = of_property_read_u32(np, "vsize", &fbdev->hsize);
+	if (ret)
+		dev_err(&pdev->dev, "Missing vsize property, assume 960\n");
+
+	/* Request I/O resource */
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!res) {
+		dev_err(&pdev->dev, "I/O resource request failed\n");
+		return -ENXIO;
+	}
+	res->flags &= ~IORESOURCE_CACHEABLE;
+	fbdev->regs = devm_ioremap_resource(&pdev->dev, res);
+	if (IS_ERR(fbdev->regs))
+		return PTR_ERR(fbdev->regs);
+
+	/* Allocate framebuffer memory */
+	fbsize = fbdev->hsize * fbdev->vsize * fbdev->frames;
+	fbdev->fb_virt = dma_alloc_coherent(&pdev->dev, PAGE_ALIGN(fbsize),
+					    &fbdev->fb_phys, GFP_KERNEL);
+	if (!fbdev->fb_virt) {
+		dev_err(&pdev->dev,
+			"Frame buffer memory allocation failed\n");
+		return -ENOMEM;
+	}
+
+	pr_debug("%s virt=%p phys=%x size=%u\n", __func__,
+		fbdev->fb_virt, (u32)fbdev->fb_phys, fbsize);
+
+	/* Clear framebuffer */
+	memset_io(fbdev->fb_virt, 0, fbsize);
+
+	fbdev->dma_tobuf = dma_request_slave_channel(&pdev->dev, "tobuf");
+	if (IS_ERR_OR_NULL(fbdev->dma_tobuf)) {
+		dev_err(&pdev->dev, "Failed to allocate DMA channel (%d).\n", ret);
+		if (fbdev->dma_tobuf)
+			ret = PTR_ERR(fbdev->dma_tobuf);
+		else
+			ret = -EPROBE_DEFER;
+		goto err_dma_free;
+	}
+	fbdev->dma_frombuf = dma_request_slave_channel(&pdev->dev, "frombuf");
+	if (IS_ERR_OR_NULL(fbdev->dma_frombuf)) {
+		dev_err(&pdev->dev, "Failed to allocate DMA channel (%d).\n", ret);
+		if (fbdev->dma_frombuf)
+			ret = PTR_ERR(fbdev->dma_frombuf);
+		else
+			ret = -EPROBE_DEFER;
+		goto err_channel_tobuf_free;
+	}
+
+	/* Setup and enable the framebuffer */
+	ovlbuf_setupfb(fbdev);
+
+	return 0;
+
+err_channel_tobuf_free:
+	dma_release_channel(fbdev->dma_tobuf);
+err_dma_free:
+	dma_free_coherent(&pdev->dev, PAGE_ALIGN(fbsize), fbdev->fb_virt,
+			  fbdev->fb_phys);
+
+	return ret;
+}
+
+static int ovlbuf_remove(struct platform_device *pdev)
+{
+	struct ovlbuf_dev *fbdev = platform_get_drvdata(pdev);
+	u32 fbsize = fbdev->hsize * fbdev->vsize * fbdev->frames;
+
+	/* Disable display */
+	ovlbuf_writereg(fbdev, OVLBUF_CONTROL, 0);
+	
+	dma_release_channel(fbdev->dma_tobuf);
+	dma_release_channel(fbdev->dma_frombuf);
+	dma_free_coherent(&pdev->dev, PAGE_ALIGN(fbsize),
+			  fbdev->fb_virt, fbdev->fb_phys);
+	return 0;
+}
+
+static struct of_device_id ovlbuf_match[] = {
+	{ .compatible = "topic,axi-overlay-mixer", },
+	{},
+};
+MODULE_DEVICE_TABLE(of, ovlbuf_match);
+
+static struct platform_driver ovlbuf_driver = {
+	.probe  = ovlbuf_probe,
+	.remove	= ovlbuf_remove,
+	.driver = {
+		.name = "axi-overlay-mixer",
+		.of_match_table = ovlbuf_match,
+	}
+};
+module_platform_driver(ovlbuf_driver);
+
+MODULE_AUTHOR("Mike Looijmans <mike.looijmans@topic.nl>");
+MODULE_DESCRIPTION("AXI Overlay buffer driver");
+MODULE_LICENSE("GPL v2");
