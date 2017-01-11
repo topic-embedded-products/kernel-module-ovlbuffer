e4c1193f04d068cecf296bc9b67bf70c4145b940
Mike Looijmans <mike.looijmans@topic.nl>
2017-01-11 15:18:01 +0100

    First working version
    Add simple code to set overlay position in logic
    gradient fill
    Get all configurables from the device-tree.
    VDMA config: reset, reduce interrupts, set frm_dly

--- ovlbuffer.c
+++ ovlbuffer.c
@@ -30,10 +30,18 @@
 #include <linux/string.h>
 #include <linux/slab.h>
 #include <linux/dmaengine.h>
-//#include <linux/dma/xilinx_dma.h>
+#include <linux/dma/xilinx_dma.h>
 
 /* Register locations */
 #define OVLBUF_CONTROL	0x00
+
+#define OVLBUF_BASE_FRAME_WIDTH 0x10
+#define OVLBUF_BASE_FRAME_HEIGHT 0x14
+
+#define OVLBUF_INSERT_WIDTH 0x20
+#define OVLBUF_INSERT_HEIGHT 0x24
+#define OVLBUF_INSERT_POSX 0x28
+#define OVLBUF_INSERT_POSY 0x2C
 
 /* Register control flags */
 #define OVLBUF_CONTROL_ENABLE 1
@@ -50,9 +58,13 @@ struct ovlbuf_dev {
 	struct dma_chan *dma_frombuf;
 	struct dma_interleaved_template *dma_template_frombuf;
 	/* Configuration */
-	u32 hsize; /* In bytes */
+	u32 hsize; /* In bytes, currently using 8bpp */
 	u32 vsize; /* Scanlines */
 	u32 frames;
+	u32 pos_x;
+	u32 pos_y;
+	u32 screen_width;
+	u32 screen_height;
 };
 
 static inline u32 ovlbuf_readreg(struct ovlbuf_dev *fbdev, loff_t offset)
@@ -65,11 +77,41 @@ static inline void ovlbuf_writereg(struct ovlbuf_dev *fbdev, loff_t offset, u32 
 	iowrite32(data, fbdev->regs + offset);
 }
 
+/**
+ * struct xilinx_vdma_config - VDMA Configuration structure
+ * @frm_dly: Frame delay
+ * @gen_lock: Whether in gen-lock mode
+ * @master: Master that it syncs to
+ * @frm_cnt_en: Enable frame count enable
+ * @park: Whether wants to park
+ * @park_frm: Frame to park on
+ * @coalesc: Interrupt coalescing threshold
+ * @delay: Delay counter
+ * @reset: Reset Channel
+ * @ext_fsync: External Frame Sync source
+ */
+static void ovlbuf_configure_vdma(struct ovlbuf_dev *fbdev)
+{
+	struct xilinx_vdma_config vdma_config;
+
+	memset(&vdma_config, 0, sizeof(vdma_config));
+	vdma_config.gen_lock = 1;
+	vdma_config.coalesc = 240; /* Get rid of useless interrupts */
+	/* Setting "reset" makes the call return immediately without applying */
+	vdma_config.reset = 1;
+	xilinx_vdma_channel_set_config(fbdev->dma_tobuf, &vdma_config);
+	xilinx_vdma_channel_set_config(fbdev->dma_frombuf, &vdma_config);
+	vdma_config.reset = 0; /* Now actually apply the settings... */
+	xilinx_vdma_channel_set_config(fbdev->dma_tobuf, &vdma_config);
+	xilinx_vdma_channel_set_config(fbdev->dma_frombuf, &vdma_config);
+}
+
 static int ovlbuf_setupfb(struct ovlbuf_dev *fbdev)
 {
 	struct dma_async_tx_descriptor *desc;
 	struct dma_interleaved_template *dma_template = fbdev->dma_template_frombuf;
-	//struct xilinx_vdma_config vdma_config;
+	u32 frame;
+	u32 fbsize = fbdev->hsize * fbdev->vsize;
 
 	/* Disable display */
 	ovlbuf_writereg(fbdev, OVLBUF_CONTROL, 0);
@@ -77,10 +119,22 @@ static int ovlbuf_setupfb(struct ovlbuf_dev *fbdev)
 	dmaengine_terminate_all(fbdev->dma_tobuf);
 	dmaengine_terminate_all(fbdev->dma_frombuf);
 
+	if (!fbdev->hsize || !fbdev->vsize) {
+		dev_info(fbdev->device, "Nul sized area, not starting.\n");
+		return 0;
+	}
+
+	/* Configure */
+	ovlbuf_writereg(fbdev, OVLBUF_BASE_FRAME_WIDTH, fbdev->screen_width);
+	ovlbuf_writereg(fbdev, OVLBUF_BASE_FRAME_HEIGHT, fbdev->screen_height);
+	ovlbuf_writereg(fbdev, OVLBUF_INSERT_WIDTH, fbdev->hsize);
+	ovlbuf_writereg(fbdev, OVLBUF_INSERT_HEIGHT, fbdev->vsize);
+	/* Quirk in logic requires us to add +1 to the position */
+	ovlbuf_writereg(fbdev, OVLBUF_INSERT_POSX, fbdev->pos_x + 1);
+	ovlbuf_writereg(fbdev, OVLBUF_INSERT_POSY, fbdev->pos_y + 1);
+
 	/* Setup VDMA address etc */
-	//memset(&vdma_config, 0, sizeof(vdma_config));
-	//vdma_config.park = 1;
-	//xilinx_vdma_channel_set_config(fbdev->dma, &vdma_config);
+	ovlbuf_configure_vdma(fbdev);
 
 	/*
 	* Interleaved DMA:
@@ -104,19 +158,22 @@ static int ovlbuf_setupfb(struct ovlbuf_dev *fbdev)
 	/* horizontal size */
 	dma_template->sgl[0].size = fbdev->hsize;
 	/* the vdma driver seems to look at icg, and not src_icg */
-	dma_template->sgl[0].icg = 0; /*  stride - hsize */
+	dma_template->sgl[0].icg = 0; /*  gap is stride - hsize */
 
-	desc = dmaengine_prep_interleaved_dma(fbdev->dma_frombuf, dma_template, 0);
-	if (!desc) {
-		pr_err("Failed to prepare from-buffer DMA descriptor\n");
-		return -ENOMEM;
+	for (frame = 0; frame < fbdev->frames; ++frame) {
+		desc = dmaengine_prep_interleaved_dma(fbdev->dma_frombuf, dma_template, 0);
+		if (!desc) {
+			dev_err(fbdev->device, "Failed to prepare from-buffer DMA descriptor %u\n", frame);
+			return -ENOMEM;
+		}
+		dmaengine_submit(desc);
+		dma_template->src_start += fbsize;
 	}
-	dmaengine_submit(desc);
 	dma_async_issue_pending(fbdev->dma_frombuf);
 
 	dma_template = fbdev->dma_template_tobuf;
 	dma_template->dir = DMA_DEV_TO_MEM;
-	dma_template->src_start = fbdev->fb_phys;
+	dma_template->dst_start = fbdev->fb_phys;
 	/* sgl list have just one entry (each interleaved frame have 1 chunk) */
 	dma_template->frame_size = 1;
 	dma_template->numf = fbdev->vsize;
@@ -127,12 +184,15 @@ static int ovlbuf_setupfb(struct ovlbuf_dev *fbdev)
 	dma_template->sgl[0].size = fbdev->hsize;
 	dma_template->sgl[0].icg = 0; /*  stride - hsize */
 
-	desc = dmaengine_prep_interleaved_dma(fbdev->dma_tobuf, dma_template, 0);
-	if (!desc) {
-		pr_err("Failed to prepare to-buffer DMA descriptor\n");
-		return -ENOMEM;
+	for (frame = 0; frame < fbdev->frames; ++frame) {
+		desc = dmaengine_prep_interleaved_dma(fbdev->dma_tobuf, dma_template, 0);
+		if (!desc) {
+			dev_err(fbdev->device, "Failed to prepare to-buffer DMA descriptor %u\n", frame);
+			return -ENOMEM;
+		}
+		dmaengine_submit(desc);
+		dma_template->dst_start += fbsize;
 	}
-	dmaengine_submit(desc);
 	dma_async_issue_pending(fbdev->dma_tobuf);
 
 	/* Enable output */
@@ -147,8 +207,12 @@ static int ovlbuf_probe(struct platform_device *pdev)
 	struct ovlbuf_dev *fbdev;
 	struct resource *res;
 	struct device_node *np = pdev->dev.of_node;
-	u32 fbsize;
 	int ret;
+	void __iomem *fb;
+	u32 fbsize;
+	u32 i;
+	u32 j;
+	u8 pixel;
 
 	fbdev = devm_kzalloc(&pdev->dev, sizeof(*fbdev), GFP_KERNEL);
 	if (!fbdev)
@@ -171,13 +235,38 @@ static int ovlbuf_probe(struct platform_device *pdev)
 	fbdev->hsize = 960;
 	fbdev->vsize = 960;
 	fbdev->frames = 3;
+	fbdev->screen_width = 1920;
+	fbdev->screen_height = 1080;
 
+	ret = of_property_read_u32(np, "num-frames", &fbdev->frames);
+	if (ret)
+		dev_warn(&pdev->dev, "Missing num-frames property, assume %u\n", fbdev->frames);
 	ret = of_property_read_u32(np, "hsize", &fbdev->hsize);
 	if (ret)
-		dev_err(&pdev->dev, "Missing hsize property, assume 960\n");
+		dev_warn(&pdev->dev, "Missing hsize property, assume %u\n", fbdev->hsize);
 	ret = of_property_read_u32(np, "vsize", &fbdev->hsize);
 	if (ret)
-		dev_err(&pdev->dev, "Missing vsize property, assume 960\n");
+		dev_warn(&pdev->dev, "Missing vsize property, assume %u\n", fbdev->vsize);
+
+	ret = of_property_read_u32(np, "screen-width", &fbdev->screen_width);
+	if (ret)
+		dev_warn(&pdev->dev, "Missing screen-width property, assume %u\n", fbdev->screen_width);
+
+	ret = of_property_read_u32(np, "screen-height", &fbdev->screen_height);
+	if (ret)
+		dev_warn(&pdev->dev, "Missing screen-height property, assume %u\n", fbdev->screen_height);
+
+	/* Put in center by default.*/
+	ret = of_property_read_u32(np, "pos-x", &fbdev->pos_x);
+	if (ret) {
+		fbdev->pos_x = (fbdev->screen_width - fbdev->hsize) / 2;
+		dev_warn(&pdev->dev, "Missing pos-x property, assume %u\n", fbdev->pos_x);
+	}
+	ret = of_property_read_u32(np, "pos-y", &fbdev->pos_y);
+	if (ret) {
+		fbdev->pos_y = (fbdev->screen_height - fbdev->vsize) / 2;
+		dev_warn(&pdev->dev, "Missing pos-y property, assume %u\n", fbdev->pos_y);
+	}
 
 	/* Request I/O resource */
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
@@ -200,11 +289,21 @@ static int ovlbuf_probe(struct platform_device *pdev)
 		return -ENOMEM;
 	}
 
-	pr_debug("%s virt=%p phys=%x size=%u\n", __func__,
+	dev_info(fbdev->device, "virt=%p phys=%#x size=%u\n",
 		fbdev->fb_virt, (u32)fbdev->fb_phys, fbsize);
 
-	/* Clear framebuffer */
-	memset_io(fbdev->fb_virt, 0, fbsize);
+	/* Fill framebuffer with gradient */
+	fb = fbdev->fb_virt;
+	for (j = 0; j < fbdev->frames; ++j)
+	{
+		pixel = 0x0;
+		dev_info(fbdev->device, "frame: %u phys=%#x\n", j, fb);
+		for (i = fbdev->vsize; i != 0; --i) {
+			memset_io(fb, pixel, fbdev->hsize);
+			++pixel;
+			fb += fbdev->hsize;
+		}
+	}
 
 	fbdev->dma_tobuf = dma_request_slave_channel(&pdev->dev, "tobuf");
 	if (IS_ERR_OR_NULL(fbdev->dma_tobuf)) {
@@ -226,7 +325,11 @@ static int ovlbuf_probe(struct platform_device *pdev)
 	}
 
 	/* Setup and enable the framebuffer */
-	ovlbuf_setupfb(fbdev);
+	ret = ovlbuf_setupfb(fbdev);
+	if (ret)
+		return ret;
+
+	dev_info(fbdev->device, "probed, buffer size: %u\n", fbsize);
 
 	return 0;
 
@@ -246,7 +349,7 @@ static int ovlbuf_remove(struct platform_device *pdev)
 
 	/* Disable display */
 	ovlbuf_writereg(fbdev, OVLBUF_CONTROL, 0);
-	
+
 	dma_release_channel(fbdev->dma_tobuf);
 	dma_release_channel(fbdev->dma_frombuf);
 	dma_free_coherent(&pdev->dev, PAGE_ALIGN(fbsize),
